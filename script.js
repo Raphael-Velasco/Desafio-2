f18fdbe40d574ad5b1ca1ea7de58fb467dcba3c0
Raphael velasco <raphaelvelasco2@gmail.com>
2022-01-18 21:41:42 -0300

    Fiz o desafio

---
+++ script.js
@@ -0,0 +1,17 @@
+let age = 19
+if(age > 18) console.log("Parabéns você é maior de idade")
+
+let myAge = 17
+let human 
+if (myAge > 18) {
+    console.log("Sou maior de idade")
+    human = true
+} else{
+    console.log("Sou menor de idade")
+    human = true
+}
+
+let aniversario = "janeiro"
+if (aniversario == "janeiro" || aniversario == "dezembro"){
+    console.log("Faço aniversario em", aniversario)
+}
